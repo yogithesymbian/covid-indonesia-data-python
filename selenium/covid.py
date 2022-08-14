a3857624e4fe3cddad372d243ac56e40030f9513
yogithesymbian <yogirenbox33@gmail.com>
2022-08-14 12:31:46 +0800

    Added WebDriverWait.

--- selenium/covid.py
+++ selenium/covid.py
@@ -1,5 +1,9 @@
 #Importing packages
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 # import pandas as pd
 from bs4 import BeautifulSoup as bsoup
 import graph as grcov
@@ -11,9 +15,18 @@ import time
 driver = webdriver.Chrome("/opt/homebrew/bin/chromedriver")
 driver.get("https://coronavirus.thebaselab.com/")
 # review = driver.find_elements_by_class_name("text-left")
+# try:
+try:
+    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, '//*[@id="country_data_rows" and text() = "Indonesia"]')))
+except:
+    print('make an error just needed the wait implicit, :D')
+# except TimeoutException:
+#     raise Exception('Unable to find text in this element after waiting 10 seconds')
+
 bs_obj = bsoup(driver.page_source, 'html.parser')
 rows = bs_obj.find_all('table', class_='table-bordered')[0].find('tbody').find_all('tr')
 
+# print(rows)
 state_label = 0
 arr_rows_data = []
 arr_rows_data_store = []
@@ -26,8 +39,8 @@ for post in rows:
         continue
     else:
         print(
-            "\n\t========================================",
-            "\n\t\tnegara         : ", country,
+            "\n\========================================",
+            "\n\t\tntegara         : ", country,
             "\n\t========================================"
         )
         for post_td_row in rows_data:
@@ -41,14 +54,14 @@ for post in rows:
             "\n\tNew Deaths         : ", arr_rows_data[3],
             "\n\tActive Cases       : ", arr_rows_data[4],
             "\n\tRecovered          : ", arr_rows_data[5],
-            "\n\tMortality Rate     : ", arr_rows_data[6],
-            "\n\tRecovery Rate      : ", arr_rows_data[7],
-            "\n\tCases per 1M Pop.  : ", arr_rows_data[8],
-            "\n\tTests per 1M Pop.  : ", arr_rows_data[9],
+            "\n\tCases per 1M Pop.  : ", arr_rows_data[6],
+            "\n\tDeaths per 1M Pop. : ", arr_rows_data[7],
+            "\n\tTests per 1M Pop.  : ", arr_rows_data[8],
+            # "\n\tTests per 1M Pop.  : ", arr_rows_data[9],
         )
         print(
             "\n\t========================================",
-            "\n\t (c) 2020 scodeid - Yogi Arif Widodo",
+            "\n\t (c) 2022 scodeid - Yogi Arif Widodo",
             "\n\t========================================"
         )
         # cstore.store_data(
@@ -59,27 +72,27 @@ for post in rows:
         #     arr_rows_data_store[4],
         #     arr_rows_data_store[5]
         # )
-        time_hhmmss = time.strftime('%H:%M:%S')
-        date_mmddyyyy = time.strftime('%d/%m/%Y')
+        # time_hhmmss = time.strftime('%H:%M:%S')
+        # date_mmddyyyy = time.strftime('%d/%m/%Y')
 
-        data = {
-            "cases": arr_rows_data_store[0],
-            "deaths": arr_rows_data_store[1],
-            "new_cases": arr_rows_data_store[2],
-            "new_deaths": arr_rows_data_store[3],
-            "active_cases": arr_rows_data_store[4],
-            "recovered": arr_rows_data_store[5],
-            "mortality_rate": arr_rows_data_store[6],
-            "recovery_rate": arr_rows_data_store[7],
-            "cases_per_1_m_pop": arr_rows_data_store[8],
-            "tests_per_1_m_pop": arr_rows_data_store[9],
-            'date': date_mmddyyyy,
-            'time': time_hhmmss,
+        # data = {
+        #     "cases": arr_rows_data_store[0],
+        #     "deaths": arr_rows_data_store[1],
+        #     "new_cases": arr_rows_data_store[2],
+        #     "new_deaths": arr_rows_data_store[3],
+        #     "active_cases": arr_rows_data_store[4],
+        #     "recovered": arr_rows_data_store[5],
+        #     "mortality_rate": arr_rows_data_store[6],
+        #     "recovery_rate": arr_rows_data_store[7],
+        #     "cases_per_1_m_pop": arr_rows_data_store[8],
+        #     "tests_per_1_m_pop": arr_rows_data_store[9],
+        #     'date': date_mmddyyyy,
+        #     'time': time_hhmmss,
 
-        }
-        firebase = firebase.FirebaseApplication('yourserver')
-        result = firebase.post('/covid/indonesia', data)
-        print(result)
+        # }
+        # firebase = firebase.FirebaseApplication('yourserver')
+        # result = firebase.post('/covid/indonesia', data)
+        # print(result)
 
         grcov.graph(
             arr_rows_data[0],
@@ -88,4 +101,4 @@ for post in rows:
             arr_rows_data[3]
         )
 
-print("debug.point")
+# print("debug.point")
